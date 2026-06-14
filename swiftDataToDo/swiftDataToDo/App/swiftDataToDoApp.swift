2e0b6028600859dc81234718489395107510b5e5
Вадим <exth331@gmail.com>
2026-06-15 01:59:25 +0300

    Add XCode file

---
+++ swiftDataToDo/swiftDataToDo/App/swiftDataToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataToDoApp.swift
+//  swiftDataToDo
+//
+//  Created by Вадим on 15.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct swiftDataToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftDataToDo/swiftDataToDo/Features/Main/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  swiftDataToDo
+//
+//  Created by Вадим on 15.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
